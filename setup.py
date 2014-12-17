4bd8eb42df03535108089c529955ad2e0237f479
maethorin <maethorin@gmail.com>
2014-12-17 16:50:29 -0200

    Versionando

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-rede',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/lojaintegrada/LI-Pagador-Rede',
     license='MIT',
     description='Meio de pagamento usando o e-Rede (https://www.userede.com.br/)',
